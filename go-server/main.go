92b51f550a452635d91e12f2be1a360c52937f1a
oni-shiro <sinchan.saha99@gmail.com>
2023-08-24 21:18:29 +0530

    Refactored

--- go-server/main.go
+++ go-server/main.go
@@ -43,11 +43,6 @@ func main() {
 	fmt.Println("Starting main function")
 	fileServer := http.FileServer(http.Dir("./static")) //returns a Handle
 	// declare the routes with handler function
-	/**
-	To understand the difference between Handle, HandlerFunc, HandleFunc
-	Blog : https://perennialsky.medium.com/understand-handle-handler-and-handlefunc-in-go-e2c3c9ecef03
-	Reddit : https://www.reddit.com/r/golang/comments/589wf4/whats_the_difference_between_httphandlefunc_and/
-	*/
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", HandleForm)
 	http.HandleFunc("/hello", HandleHello)
